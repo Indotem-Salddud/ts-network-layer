f51f285dc7505ee33fe4264be1a5b4d3a7442f72
Daniel Bañobre Dopico <danielbanobre@indotem.com>
2021-12-13 10:49:54 +0100

    TS folder structure

---
+++ src/interfaces/models/manuDraft.ts
@@ -0,0 +1,25 @@
+interface NetworkClient {
+}
+
+interface HomeRouter {
+    gotoDetailView(): void
+}
+
+class Home {
+    client: NetworkClient;
+    router: HomeRouter;
+    constructor(client: NetworkClient, router: HomeRouter){
+        this.client = client;
+        this.router = router;
+    }
+}
+
+// abstract factory
+class RootFactory {
+    static home (client: NetworkClient, router: HomeRouter): Home {
+        const scene = new Home(client, router);
+        return scene;
+    }
+}
+
+RootFactory.home()
