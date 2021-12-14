f50ae6dff6d7cfb0887a8e90170a4bc5e0dc6992
Daniel Bañobre Dopico <danielbanobre@indotem.com>
2021-12-14 13:22:42 +0100

    First interfaces draft

---
+++ src/interfaces/models/models.ts
@@ -0,0 +1,63 @@
+enum HTTPMethods {
+    get = 'GET',
+    post = 'POST',
+    put = 'PUT',
+    patch = 'PATCH',
+    delete = 'DELETE',
+    options = 'OPTIONS',
+  }
+  
+  enum Protocols {
+    http = 'http',
+    https = 'https',
+    mqtt = 'mqtt',
+  }
+  
+  interface EndPoint {
+    protocol: Protocols;
+    addr: string;
+    port: number;
+    resourcePath: string;
+  }
+  
+  interface RequesHeaders {
+    //TODO
+  }
+  
+  interface APIRequest {
+    method?: HTTPMethods;
+    headers?: RequesHeaders;
+    body?: BodyInit;
+  }
+  
+  interface ResponseCache {
+    cacheable: boolean;
+    maxAge?: number;
+    body?: BodyInit;
+    update(callbacks: APICallbacks): void;
+  }
+  
+  interface APICallbacks {
+    // TODO
+  }
+  
+  interface NetworkQuery {
+    host: EndPoint;
+    request: APIRequest;
+    callbacks: APICallbacks;
+    cache: ResponseCache;
+    send(): void;
+  }
+  
+  /* Aplication examples:
+  
+  const MapsAPI = new EndPoint("apis.google.com","/maps/");
+  const getMaps = new NetworkQuery(MapsAPI);
+  getMaps.send();
+  
+  const StoreAPI =  new EndPoint("api.littleshop.com");
+  const postProductRequest = ({method: "POST", body: newProduct, }})
+  const postProduct = new NetworkQuery(MapsAPI,postProductRequest)
+  
+  */
+  
